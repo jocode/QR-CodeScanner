c5d518215eb09e06a83828ba9467b2c8d9a0cc24
Johan Mosquera <joncamilomoni@gmail.com>
2023-08-23 14:50:39 -0500

    QR CodeScanner App - Using zxing

--- app/src/main/java/com/example/qr_codescanner/MainActivity.kt
+++ app/src/main/java/com/example/qr_codescanner/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.qr_codescanner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Size
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.example.qr_codescanner.ui.theme.QRCodeScannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +40,106 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QRCodeScannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+
+                var code by remember {
+                    mutableStateOf("")
+                }
+                val context = LocalContext.current
+                val lifecycleOwner = LocalLifecycleOwner.current
+                val cameraProviderFuture = remember {
+                    ProcessCameraProvider.getInstance(context)
+                }
+
+
+                /**
+                 * Make sure that the user has granted permission to use the camera.
+                 */
+                var hasCameraPermission by remember {
+                    mutableStateOf(
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                }
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { granted ->
+                        hasCameraPermission = granted
+                    }
+                )
+                LaunchedEffect(key1 = true) {
+                    if (!hasCameraPermission) {
+                        launcher.launch(Manifest.permission.CAMERA)
+                    }
+                }
+
+
+
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Greeting("Android")
+
+                    if (hasCameraPermission) {
+                        AndroidView(
+                            factory = { context ->
+                                val previewView = PreviewView(context)
+                                val preview = Preview.Builder().build()
+                                val selector = CameraSelector.Builder()
+                                    .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                    .build()
+
+                                preview.setSurfaceProvider(previewView.surfaceProvider)
+                                val imageAnalysis = ImageAnalysis.Builder()
+                                    .setTargetResolution(
+                                        Size(
+                                            previewView.width,
+                                            previewView.height
+                                        )
+                                    )
+                                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                                    .build()
+
+                                imageAnalysis.setAnalyzer(
+                                    ContextCompat.getMainExecutor(context),
+                                    QrCodeAnalyzer { qrCodes ->
+                                        code = qrCodes
+                                    }
+                                )
+
+                                try {
+                                    cameraProviderFuture.get().bindToLifecycle(
+                                        lifecycleOwner,
+                                        selector,
+                                        preview,
+                                        imageAnalysis
+                                    )
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
+
+                                previewView
+
+                            },
+                            modifier = Modifier
+                                .weight(1f)
+                        )
+
+                        Text(
+                            text = code,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(top = 32.dp)
+                                .fillMaxWidth()
+                                .padding(32.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QRCodeScannerTheme {
-        Greeting("Android")
     }
 }
